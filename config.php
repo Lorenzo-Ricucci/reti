7f4b2e9ed0771c764ed80e1251d9a7fcff9196d6
Lorenzo-Ricucci <165045049+Lorenzo-Ricucci@users.noreply.github.com>
2024-04-03 19:25:20 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+
+
+<?php 
+
+$connectionInfo = array("UID" => "lorenzo", "pwd" => "Moretti123!", "Database" => "sito", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:serversito.database.windows.net,1433";
+
+?>
+
+
