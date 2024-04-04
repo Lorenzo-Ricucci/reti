a975010ce057c01a080187cc4fe615d45bd5ba4b
Lorenzo-Ricucci <165045049+Lorenzo-Ricucci@users.noreply.github.com>
2024-04-04 10:54:22 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 
-
 <?php 
 
-$connectionInfo = array("UID" => "lorenzo", "pwd" => "Moretti123!", "Database" => "sito", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "lorenzo", "pwd" => "Password1234!", "Database" => "sito", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:serversito.database.windows.net,1433";
 
 ?>
